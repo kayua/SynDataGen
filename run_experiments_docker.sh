305b6e3a59407d976188006559d3f45dfc703535
AngeloAnon266 <angelodiniz44@gmail.com>
2025-08-06 17:40:29 -0300

    Add files via upload

--- run_experiments_docker.sh
+++ run_experiments_docker.sh
@@ -12,19 +12,25 @@ if docker info >/dev/null 2>&1; then
         docker build -t sf25/maldatagen:latest .
     fi
     
-    docker run -it --name=MalDataGen-$RANDOM \
-        -v $DIR:/MalDataGen/Scripts \
-        -e DISPLAY=unix$DISPLAY \
-        sf25/maldatagen:latest \
-        python3 run_campaign_sbseg.py
-
+    # Check if output file is provided as first argument
+    if [ -z "$1" ]; then
+        # No output file specified - use interactive mode
+        docker run -it --name=MalDataGen-$RANDOM \
+            -v $DIR:/MalDataGen/Scripts \
+            -e DISPLAY=unix$DISPLAY \
+            sf25/maldatagen:latest \
+            python3 run_campaign_sbseg.py
+    else
+        # Output file specified - redirect output
+        OUTPUT_FILE="$1"
+        docker run --name=MalDataGen-$RANDOM \
+            -v $DIR:/MalDataGen/Scripts \
+            -e DISPLAY=unix$DISPLAY \
+            sf25/maldatagen:latest \
+            python3 run_campaign_sbseg.py > "$OUTPUT_FILE" 2>&1
+    fi
     
 else
     echo "Docker permission error. Run this command and restart your machine:"
     echo "sudo usermod -aG docker $USER"
 fi
-
-
-
-
-
